a05b48f478a6866a495b22695422ecba9baf378a
Aaron Hill <aaronthill16@gmail.com>
2016-04-16 16:45:39 -0400

    Displays board after each human turn.

--- Tic_Tac_Toe_Human.rb
+++ Tic_Tac_Toe_Human.rb
@@ -4,9 +4,25 @@ class Human
 
 	def get_move(board)
 		array = get_available_spaces(board)
+		display_board(board)
 		puts "Choose a space from the following: #{array}."
 		answer = gets.chomp
 		move = answer.to_i
+		if valid_position?(array, move-1)
+			move = answer.to_i
+		else
+		end
+	end
+
+	def display_board(board)
+		puts """
+		Game Board 
+ 		 #{board[0]} | #{board[1]} | #{board[2]} 
+		-----------
+ 		 #{board[3]} | #{board[4]} | #{board[5]} 
+		-----------
+ 		 #{board[6]} | #{board[7]} | #{board[8]} 
+ 		"""
 	end
 
 end
